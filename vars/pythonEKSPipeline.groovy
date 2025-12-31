4a7a106dcf7d2a0143061a1d9f2a7c65dc81cf96
sathishdevops38 <sathishdevops38@gmail.com>
2025-12-31 14:37:19 +0530

    deploy

--- vars/pythonEKSPipeline.groovy
+++ vars/pythonEKSPipeline.groovy
@@ -174,19 +174,19 @@ def call(Map configMap){
                     }
                 }
             }
-            stage('Trigger DEV Deploy') {
-                steps {
-                    script {
-                        build job: "../${COMPONENT}-deploy",
-                            wait: false, // Wait for completion
-                            propagate: false, // Propagate status
-                            parameters: [
-                                string(name: 'appVersion', value: "${appVersion}"),
-                                string(name: 'deploy_to', value: "dev")
-                            ]
-                    }
-                }
-            }
+            // stage('Trigger DEV Deploy') {
+            //     steps {
+            //         script {
+            //             build job: "../${COMPONENT}-deploy",
+            //                 wait: false, // Wait for completion
+            //                 propagate: false, // Propagate status
+            //                 parameters: [
+            //                     string(name: 'appVersion', value: "${appVersion}"),
+            //                     string(name: 'deploy_to', value: "dev")
+            //                 ]
+            //         }
+            //     }
+            // }
         }
         post{
             always{
